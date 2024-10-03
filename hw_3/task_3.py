ab166378ec1872cf7a968fb76624009ed126bdd3
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2024-10-03 21:56:26 +0300

    Added description to task 3 HW 3

--- hw_3/task_3.py
+++ hw_3/task_3.py
@@ -8,17 +8,19 @@
 
 numbers = [1, 2, 3, 4, 5]
 
-
+# Если массив у нас пустой, то создаем 2 пустых массива
 if len(numbers) == 0:
     first_mass = numbers[:len(numbers)]
     second_mass = numbers[:len(numbers)]
     result = [first_mass, second_mass]
     print(result)
+# Если массив четный, делим его пополам и разбиваем на 2 половины
 elif len(numbers) % 2 == 0:
     first_mass = numbers[:len(numbers) // 2]
     second_mass = numbers[len(numbers) // 2:]
     result = [first_mass, second_mass]
     print(result)
+# Если массив нечетный, делим его, добавляем один элемент к первой половине.
 elif len(numbers) % 2 != 0:
     first_mass = numbers[:(len(numbers) + 1) // 2]
     second_mass = numbers[(len(numbers) + 1) // 2:]
