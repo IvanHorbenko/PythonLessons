1ef04c130070d19580442046a168756803ef1fb7
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2024-10-03 20:21:18 +0300

    Fixed issue with empty list

--- hw_3/task_3.py
+++ hw_3/task_3.py
@@ -6,10 +6,15 @@
 # [] => [[], []]
 
 
-numbers = [1]
+numbers = []
 
 
-if len(numbers) % 2 == 0:
+if len(numbers) == 0:
+    first_mass = numbers[:len(numbers)]
+    second_mass = numbers[:len(numbers)]
+    result = first_mass, second_mass
+    print(result)
+elif len(numbers) % 2 == 0:
     first_mass = numbers[:len(numbers) // 2]
     second_mass = numbers[len(numbers) // 2:]
     result = first_mass, second_mass
@@ -20,10 +25,6 @@ elif len(numbers) % 2 != 0:
     second_mass = numbers[(len(numbers) + 1) // 2:]
     result = first_mass, second_mass
     print(result)
-else:
-    first_mass = numbers[:len(numbers)]
-    second_mass = numbers[:len(numbers)]
-    result = first_mass, second_mass
-    print(result)
+
 
 
