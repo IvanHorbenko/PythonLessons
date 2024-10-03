05f8d758655ee3fef98aded1d11ac4e284a396d6
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2024-10-03 20:46:13 +0300

    Fixed issue with []

--- hw_3/task_3.py
+++ hw_3/task_3.py
@@ -6,24 +6,23 @@
 # [] => [[], []]
 
 
-numbers = []
+numbers = [1, 2, 3, 4, 5]
 
 
 if len(numbers) == 0:
     first_mass = numbers[:len(numbers)]
     second_mass = numbers[:len(numbers)]
-    result = first_mass, second_mass
+    result = [first_mass, second_mass]
     print(result)
 elif len(numbers) % 2 == 0:
     first_mass = numbers[:len(numbers) // 2]
     second_mass = numbers[len(numbers) // 2:]
-    result = first_mass, second_mass
+    result = [first_mass, second_mass]
     print(result)
-
 elif len(numbers) % 2 != 0:
     first_mass = numbers[:(len(numbers) + 1) // 2]
     second_mass = numbers[(len(numbers) + 1) // 2:]
-    result = first_mass, second_mass
+    result = [first_mass, second_mass]
     print(result)
 
 
