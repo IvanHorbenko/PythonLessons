7e5e16fc45c94304bbdfb83ac009d288e217437a
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2024-10-22 14:19:36 +0300

    Added HW 5 task 1

--- hw_5/task_1.py
+++ hw_5/task_1.py
@@ -1,50 +1,37 @@
-text = "Should, I. subscribe? Yes!"
+import string
+import keyword
 
+test_date =  [ "_", "__", "___", "x", "get_value", "get value", "get!value", "some_super_puper_value",
+            "Get_value", "get_Value", "getValue", "3m", "m3", "assert", "assert_exception" ]
 
-# isalpha(): повертає True, якщо рядок складається лише з алфавітних символів
+for variable_name in test_date:
+    print(f"{variable_name}: ", end="")
 
-print(text.isalpha())
+    # Проверка, что переменная не начинается с цифры
+    if variable_name[0].isdigit():
+        print(False)
+        continue
 
-# islower(): повертає True, якщо рядок складається лише із символів у нижньому регістрі
+    # Проверка на количество подчеркиваний (не более одного)
+    if variable_name.count('_') > 1:
+        print(False)
+        continue
 
-print(text.islower())
+    # Проверка на использование ключевого слова
+    if variable_name in keyword.kwlist:
+        print(False)
+        continue
 
-# isupper(): повертає True, якщо всі символи рядка у верхньому регістрі
+    # Допустимые символы: строчные буквы, цифры и одно подчеркивание
+    valid_chars = string.ascii_lowercase + string.digits + '_'
 
-print(text.isupper())
+    # Проверка каждого символа
+    invalid_char = False
+    for char in variable_name:
+        if char not in valid_chars:
+            print(False)
+            invalid_char = True
+            break
 
-# isdigit(): повертає True, якщо всі символи рядка - цифри
-
-print(text.isdigit())
-
-# isnumeric(): повертає True, якщо рядок є числом
-
-print(text.isnumeric())
-
-# startswith(str): повертає True, якщо рядок починається з підрядка str
-
-print(text.startswith("helLo"))
-
-# endswith(str): повертає True, якщо рядок закінчується на підрядок str
-
-print(text.endswith("D"))
-
-# lower(): перекладає рядок у нижній регістр
-
-print(text.lower())
-
-# upper(): перекладає рядок у верхній регістр
-
-print(text.upper())
-
-# title(): початкові символи всіх слів у рядку перекладаються у верхній регістр
-
-print(text.title())
-
-# capitalize(): перекладає у верхній регістр першу літеру тільки першого слова рядка
-
-print(text.capitalize())
-
-
-
-
+    if not invalid_char:
+        print(True)
