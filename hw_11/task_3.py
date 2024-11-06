113dc0ab6264e40f11a1b46bb34b54a4dbf08d34
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2024-11-06 14:05:31 +0200

    Added HW 11 task 3

--- hw_11/task_3.py
+++ hw_11/task_3.py
@@ -2,7 +2,7 @@ def is_even(number):
     return (number & 1) == 0
 
 
-assert is_even(2494563894038**2) == True, 'Test1'
-assert is_even(1056897**2) == False, 'Test2'
-assert is_even(24945638940387**3) == False, 'Test3'
+assert is_even(2494563324234324234324894038**2) == True, 'Test1'
+assert is_even(234234234056897**2) == False, 'Test2'
+assert is_even(2433333333945638940387**3) == False, 'Test3'
 
